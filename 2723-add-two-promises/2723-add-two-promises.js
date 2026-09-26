d68a237c3a6f31b720737deb7ea6f23ec0324159
Moyin Pasha <moyinpasha51@gmail.com>
2026-09-26 18:25:06 +0530

    Time: 61 ms (19.28%), Space: 54.4 MB (34%) - LeetHub

--- 2723-add-two-promises/2723-add-two-promises.js
+++ 2723-add-two-promises/2723-add-two-promises.js
@@ -4,8 +4,7 @@
  * @return {Promise}
  */
 var addTwoPromises = async function (promise1, promise2) {
-    const [data1, data2] = await Promise.all([promise1, promise2])
-    return data1 + data2;
+    return Promise.all([promise1,promise2]).then(([data1,data2])=>data1+data2)
 };
 
 /**
